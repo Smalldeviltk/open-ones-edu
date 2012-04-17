969bde88dc8d951cb5118b5931f9f7c9d2392b25
onlyhope98@gmail.com <onlyhope98@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-17 08:30:31 +0000

    Họ tên: Tạ Khương Quang

---
+++ trunk/Add2N-Console/SourceCode/11CSharp/[11]_Add2N_Tạ Khương Quang/[11]_Add2N/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _11__Add2N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        c:
+                Console.Clear();
+                View.Introduce();
+                View.ThongTin(Logic.Result());
+                Console.WriteLine("\n**************************************\nBan co muon tiep tuc khong? 0. Khong\t1.Co");
+                Console.Write("\t\tChon: ");
+                string t = Console.ReadLine().Trim();
+                if (t.Equals("1") || t.ToLowerInvariant().Equals("co"))
+                    goto c;
+        }
+    }
+}
