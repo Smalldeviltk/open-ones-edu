e519973dfb24af41e35ec6e807182eaa038c35c1
vuvuonghiep <vuvuonghiep@gmail.com>
2012-04-17 14:17:22 +0000

    Vu Vuong Hiep created Number structure & constructors

--- Add2N-Console/SourceCode/25CPlus/Add2N/number.h
+++ Add2N-Console/SourceCode/25CPlus/Add2N/number.h
@@ -11,12 +11,16 @@
  *					16.04.12	-	Doan Quang Khoi		Create subNumber class
  *														Description: Store a sequential number
  *
+ *					16.04.12	-	Vu Vuong Hiep		Create Number class
+ *														Description: create structure and constructors
  * --
 ***********************************************************************************************************/
 
 
 #include <stdio.h>
+#include <iostream>
 
+using namespace std;
 class subNumber { //subNumber in a sequent of number
 public:
 	char num;
@@ -48,4 +52,44 @@ public:
 		return (num - '0');
 	}
 };
+
+
+class Number { //Link list contain sequence of subNumber items
+public:
+	subNumber *pHead, *pTail; //Head & Tail of the link list
+	int fail;
+
+	Number(const char *Num) { //Constructor with a char array
+		fail = 0;
+		const char *oNum = Num;
+		pHead = pTail = NULL;
+
+		while(Num[0] >= '0' && Num[0] <= '9') {
+			subNumber *num = new subNumber(*(Num++));
+			AddTail(num);
+		}
+
+		if (Length() == 0 || ((Num[0] < '0' || Num[0] > '9') && Num[0] != '\0' )) { //Check the value
+			printf("%s khong hop le\r\n", oNum);
+			fail = 1;
+		}
+	}
+
+	Number() { //Default constructor
+		fail = 0;
+		pHead = pTail = NULL;
+	}
+
+	int Length() { //Calculate the number of the elements
+		int length = 0;
+		subNumber *current = pHead;
+		while(current != NULL) {
+			current = current->pNext;
+			length++;
+		}
+		return length;
+	}
+
+};
+
 
