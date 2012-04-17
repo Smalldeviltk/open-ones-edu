e7c1d824ed3297d0e33008922223050e5f3e9eb4
nguyentanhoan1002 <nguyentanhoan1002@gmail.com>
2012-04-17 06:06:45 +0000

    NHOM 11_NGUYEN TAN HOAN

---
+++ Add2N-Console/SourceCode/11CSharp/[11]_NGUYEN TAN HOAN_Add2N/[11]_Add2N/View.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _11__Add2N
+{
+    class View
+    {
+        // Ham neu cau truc de thuc hien chuong trinh
+        public static void Introduce()
+        {
+            Console.WriteLine("\n\n******CHUONG TRINH CONG 2 SO NGUYEN LON ******");
+            Console.WriteLine("Nhap thong tin theo cau truc: Add2N [So1] [So2]");
+        }
+
+        // Ham nhap thong tin va tra ve chuoi thong tin co ban
+        public static string[] NhapThongTin()
+        {
+            string strTempt;
+            string[] temp; 
+            Console.Write("\n\tNHAP THONG TIN: ");
+            strTempt = Console.ReadLine().Trim();
+            temp = strTempt.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return temp;
+        }
+
+        // Ham hien thi thong tin
+        public static void ThongTin(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
