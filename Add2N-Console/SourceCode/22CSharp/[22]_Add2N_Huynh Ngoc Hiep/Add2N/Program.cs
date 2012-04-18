3d8eb83e21796dc2f900eca26fd63a47cb34a0ee
Mr.hiepuit <Mr.hiepuit@gmail.com>
2012-04-18 03:25:13 +0000

    huynh ngoc hiep

---
+++ Add2N-Console/SourceCode/22CSharp/[22]_Add2N_Huynh Ngoc Hiep/Add2N/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Add2N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "exit", a = "";
+            while(s!=a)
+            {
+                a = Logic.KiemTra(View.Nhap());
+                Console.WriteLine(a);
+                if (Logic.kt && a!="exit") View.Xuat(View.Input, Logic.strOutput(View.Input));
+            }
+        }
+    }
+}
