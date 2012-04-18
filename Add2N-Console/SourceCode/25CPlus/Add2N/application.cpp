1cbe93c4f51a5d05f10582b644409976efae5e3e
dark.knight.ck5 <dark.knight.ck5@gmail.com>
2012-04-18 13:24:00 +0000

    Supporting parameters

--- Add2N-Console/SourceCode/25CPlus/Add2N/application.cpp
+++ Add2N-Console/SourceCode/25CPlus/Add2N/application.cpp
@@ -6,6 +6,24 @@
  * Description:		Main application
  *					Start the application with parameters
  * 
- * Mod. History:	16.04.12	-	Doan Quang Khoi		File First Created					
+ * Mod. History:	16.04.12	-	Doan Quang Khoi		File First Created
+ *
+ *					18.04.12	-	Tran Minh Thanh		Create main
+ *														Description: Supporting parameters				
  * --
  **********************************************************************************************************/
+
+
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include "number.h"
+
+using namespace std;
+
+void main(int argc, const char* argv[]) {
+	Number *N1, *N2;
+	if (argc == 3) {
+		//Loop the parameters with index 1, 2
+	}
+}
