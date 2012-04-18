1b96a8e7b0e0b3d7047e8c14a51b9df33ef4e487
anhtienas <anhtienas@gmail.com>
2012-04-18 13:31:22 +0000

    Analyze and output the result

--- Add2N-Console/SourceCode/25CPlus/Add2N/application.cpp
+++ Add2N-Console/SourceCode/25CPlus/Add2N/application.cpp
@@ -9,7 +9,10 @@
  * Mod. History:	16.04.12	-	Doan Quang Khoi		File First Created
  *
  *					18.04.12	-	Tran Minh Thanh		Create main
- *														Description: Supporting parameters				
+ *														Description: Supporting parameters
+ *
+ *					18.04.12	-	Tran Duc Tien		Create main					
+ *														Description: Analyze and output the result					
  * --
  **********************************************************************************************************/
 
@@ -24,6 +27,14 @@ using namespace std;
 void main(int argc, const char* argv[]) {
 	Number *N1, *N2;
 	if (argc == 3) {
-		//Loop the parameters with index 1, 2
+		N1 = new Number(argv[1]); //First number created from 2nd parameter
+		N2 = new Number(argv[2]); //Second number created from 3rd parameter
+
+		//Calculate the value
+		Number Result = *N1 + *N2;
+		
+		//Check fail
+		if (!N1->fail && !N2->fail)
+			cout<<argv[1]<<" + "<<argv[2]<<" = "<<Result; //Output the value
 	}
 }
