39edf6e14b28437bc14cd5e0531bdf12acf0ee06
vantien.uit05 <vantien.uit05@gmail.com>
2012-04-18 03:58:06 +0000

    em đã hoàn thành phần thư viện của chArray.h phần chArray.cpp sẽ up nhanh.. các anh lấy mà dùng nha.

---
+++ Add2N-Console/SourceCode/62CPlus/Add2N/Add2N/chArray.h
@@ -0,0 +1,23 @@
+/**********************************************************
+Author:           Truong Van Linh
+Module Name:      chArray.cpp
+Description:      include some method what work with an array of char
+
+*********************************************************/
+
+
+#ifndef CHARRAY_H
+#define CHARRAY_H
+
+class chArray
+{
+private:
+	char *pchar; 
+	int wNumChars;
+public:
+	chArry(int);
+	void invertChars();
+	void showChars();
+	int wgetNumChars();
+}
+#endif
