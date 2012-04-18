6ff3d62d836f079c1e9c24a947e0efcc62c60fb6
Mr.hiepuit@gmail.com <Mr.hiepuit@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-18 03:25:13 +0000

    huynh ngoc hiep

---
+++ trunk/Add2N-Console/SourceCode/22CSharp/[22]_Add2N_Huynh Ngoc Hiep/Add2N/View.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Add2N
+{
+    class View
+    {
+        public static string[] Input;
+        public static string[] Nhap()
+        {
+            Input= Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            return Input;
+        }
+        public static void Xuat(string[] strInput, string strOutput)
+        {
+            Console.WriteLine(string.Format("{0} + {1} = {2}", strInput[1], strInput[2], strOutput));
+        }
+    }
+}
