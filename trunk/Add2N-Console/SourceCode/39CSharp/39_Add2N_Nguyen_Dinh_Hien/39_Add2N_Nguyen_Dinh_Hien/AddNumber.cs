48d0886464ac967945b5b307e6fb7e35292181b9
thehien.nguyen00@gmail.com <thehien.nguyen00@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-19 17:29:41 +0000

    Nguyen Dinh Hien lam phan ConvertToChar

---
+++ trunk/Add2N-Console/SourceCode/39CSharp/39_Add2N_Nguyen_Dinh_Hien/39_Add2N_Nguyen_Dinh_Hien/AddNumber.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _39_ADD2N
+{
+    class AddNumber
+    {
+        /// <summary>
+        ///   include 4 methods 
+        ///   check string, use to check the string input. will return true or false if string is valid or not
+        ///   Convert char[] to string,
+        ///   Convert string to char[]
+        ///   Add 2 strings
+        /// </summary>
+        /// 
+        //Pham hoang phu
+
+        public bool IsValid(string strChecked)
+        {
+            char[] chrTemp = new char[strChecked.Length];
+            chrTemp = strChecked.ToCharArray();
+            for (int i = 0; i < chrTemp.Length; i++)
+            {
+                if (chrTemp[i] < '0' || chrTemp[i] > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //Van Trung Duy
+        public string ConvertToString(char[] chrIn)
+        {
+            char[] chrOut = new char[chrIn.Length];
+            int j = chrIn.Length - 1;
+            for (int i = 0; i < chrIn.Length; i++)
+            {
+                chrOut[i] = chrIn[j];
+                j--;
+            }
+            return new string(chrOut);
+        }
+
+        //Nguyen Dinh Hien
+        public char[] ConverToChar(string strIn, int intLength)
+        {
+            char[] chrIn = new char[strIn.Length];
+            chrIn = strIn.ToCharArray();
+            char[] chrOut = new char[intLength];
+            int j = 0;
+            for (int i = chrIn.Length - 1; i >= 0; i--)
+            {
+                chrOut[j] = chrIn[i];
+                j++;
+            }
+            for (; j < intLength; j++)
+            {
+                chrOut[j] = '0';
+            }
+            return chrOut;
+        }
+
+        //Nguyen Thanh Sang
+        public string AddTwoStrings(string strFirstOperator, string strSecondOperator)
+        {
+            char[] chrFirstOprator = new char[strFirstOperator.Length];
+            char[] chrSecondOperator = new char[strSecondOperator.Length];
+            int intResultLength = ((strFirstOperator.Length < strSecondOperator.Length) ? strSecondOperator.Length : strFirstOperator.Length);
+            chrFirstOprator = ConverToChar(strFirstOperator, intResultLength);
+            chrSecondOperator = ConverToChar(strSecondOperator, intResultLength);
+            char[] chrResult = new char[intResultLength];
+            int intOdd = 0;
+            for (int i = 0; i < intResultLength; i++)
+            {
+                int intAdd = ((int)chrFirstOprator[i] + (int)chrSecondOperator[i]) + intOdd;
+                if (intAdd < 106)
+                {
+                    intOdd = 0;
+                    chrResult[i] = (char)(intAdd - '0');
+                }
+                else
+                {
+                    chrResult[i] = (char)(intAdd - '0' - 10);
+                    intOdd = 1;
+                }
+            }
+            if (intOdd == 1)
+            {
+                char[] chrNewResult = new char[intResultLength + 1];
+                for (int i = 0; i < chrResult.Length; i++)
+                {
+                    chrNewResult[i] = chrResult[i];
+                    chrNewResult[intResultLength] = '1';
+                }
+                return ConvertToString(chrNewResult);
+            }
+            else
+            {
+                return ConvertToString(chrResult);
+            }
+        }
+
+    }
+}
---
+++ trunk/Add2N-Console/SourceCode/39CSharp/39_Add2N_Nguyen_Dinh_Hien/39_Add2N_Nguyen_Dinh_Hien/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _39_ADD2N
+{
+    /// <summary>
+    ///  use class AddNumber to add 2 strings what use define ADD2N string1 string2 
+    ///
+    /// </summary>
+    class Program
+    {
+        //Trinh Duy Thanh
+        static void Main()
+        {
+            Console.WriteLine("Example: ADD2N 1111 2222 \nReSult: 1111 + 2222= 3333 ");
+            Console.WriteLine("Input Your String Here:  ");
+            string strInput = Console.ReadLine();
+            string[] strSplitInput;
+            strSplitInput = strInput.Split(' ');
+            AddNumber Add = new AddNumber();
+            if (Add.IsValid(strSplitInput[1]) && Add.IsValid(strSplitInput[2]))
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} + {1} = {2}", strSplitInput[1], strSplitInput[2], Add.AddTwoStrings(strSplitInput[1],
+                                                                                                            strSplitInput[2]));
+            }
+            else
+            {
+                if (!Add.IsValid(strSplitInput[1]) && !Add.IsValid(strSplitInput[2]))
+                {
+                    Console.WriteLine("{0}, {1} are not valid string", strSplitInput[1], strSplitInput[2]);
+                }
+                else
+                {
+                    if (!Add.IsValid(strSplitInput[1]))
+                    {
+                        Console.WriteLine("{0} is not valid string", strSplitInput[1]);
+                    }
+                    if (!Add.IsValid(strSplitInput[2]))
+                    {
+                        Console.WriteLine("{0} is not valid string", strSplitInput[2]);
+                    }
+                }
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
