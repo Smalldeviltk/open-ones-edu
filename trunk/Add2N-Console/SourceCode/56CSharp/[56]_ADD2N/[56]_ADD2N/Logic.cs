320ab0b3fc4e4cae7d43a769a0fcf7d16ccac6b9
heyvin.nouhengy@gmail.com <heyvin.nouhengy@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-19 14:50:14 +0000

    Xong phan Kiem tra chuoi nhap vao nhe

--- trunk/Add2N-Console/SourceCode/56CSharp/[56]_ADD2N/[56]_ADD2N/Logic.cs
+++ trunk/Add2N-Console/SourceCode/56CSharp/[56]_ADD2N/[56]_ADD2N/Logic.cs
@@ -20,7 +20,32 @@ namespace _56__ADD2N
         //Hàm kiểm tra chuỗi có hợp lê, có phải là chuỗi số nguyên hay không
         public static bool KiemTraChuoi(string[] s)
         { 
-           
+           try
+            {
+                string s1 = s[0];
+                string s2 = s[1];
+                string ChuoiSai;
+                bool test1 = SoNguyen(s1);
+                bool test2 = SoNguyen(s2);
+
+                if (!test1 || !test2)
+                {
+                    if (!test1)
+                    {
+                        ChuoiSai = s1;
+                        if (!test2) ChuoiSai += " va " + s2;
+                    }
+                    else ChuoiSai = s2;
+                    View.BaoLoi(ChuoiSai);
+                    return false;
+                }
+                return true;
+            }
+            catch
+            {
+                View.BaoLoi("Nhap");
+                return false;
+            }
         }
 
         //Hàm tổng 2 số nguyên từ người nhập
