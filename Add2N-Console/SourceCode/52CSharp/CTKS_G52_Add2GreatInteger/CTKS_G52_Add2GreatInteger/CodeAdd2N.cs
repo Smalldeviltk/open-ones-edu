a8989e1f945cd386ecab8a2da1ab76ede598a177
trdangkhoa1900 <trdangkhoa1900@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-19 18:36:04 +0000

    sửa lại do project bị hỏng

---
+++ Add2N-Console/SourceCode/52CSharp/CTKS_G52_Add2GreatInteger/CTKS_G52_Add2GreatInteger/CodeAdd2N.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTKS_GreatInteger
+{
+    struct Error
+    {
+        bool KeyWord;
+        bool Space;
+        bool Num1;
+        bool Num2;
+        bool NoError;
+    }
+    class CodeAdd2N
+    {
+        public string Code { get; set;}
+        public bool Valid { get; set; }
+        //constructor
+        public CodeAdd2N()
+        {
+            Code = "Add2N 1 1";
+            Valid = true;
+
+        }
+        public CodeAdd2N(string _Code)
+        {
+            Code = _Code;
+
+        }
+
+
+        //Valid or Invalid
+        public string GetError()
+        {
+            int space=0;
+            for (int i = 0; i < Code.Length; i++)
+            {
+                if (Code[i] == ' ')
+                {
+                    space++;
+                }
+            }
+            //Wrong if KeyWord isn't "Add2N"
+            if(Code.Remove(6)!="Add2N ")
+            {
+                return "KeyWord";
+            }
+            //wrong if the other 2 spacious
+            if (space != 2)
+            {
+                return "Space";
+            }
+                return "Right";
+        }
+
+        //Get number 1
+        public string Number1()
+        {
+            string s="";
+            for (int i = 6; Code[i]!=' '; i++)
+            {
+                s += Code[i];
+            }
+
+            return s;
+        }
+        //Get number 2
+        public string Number2()
+        {
+            string rs = "";
+            for (int i = Code.Length-1; Code[i] != ' '; i--)
+            {
+                rs += Code[i];
+            }
+            string s="";
+            for (int i = rs.Length - 1; i >= 0; i--)
+            {
+                s += rs[i];
+            }
+            
+            return s;
+        }
+
+    
+
+
+    }
+}
