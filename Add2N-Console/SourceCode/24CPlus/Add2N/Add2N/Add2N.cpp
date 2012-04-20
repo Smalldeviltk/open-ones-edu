3c161150cd9d21e7236bce7287a5a4ae45730bde
dinhim_th <dinhim_th@yahoo.com>
2012-04-20 07:48:56 +0000

    thuy sua lai ham TestInputValid

--- Add2N-Console/SourceCode/24CPlus/Add2N/Add2N/Add2N.cpp
+++ Add2N-Console/SourceCode/24CPlus/Add2N/Add2N/Add2N.cpp
@@ -129,22 +129,32 @@ void Add2N::GetSumOfNumber()
 bool Add2N::TestInputValid( char *s )
 {
 	int wX;
+	bool temp=true;
 
 	for(int i = 0; i < strlen(s); i++)
-		if (s[i] != '0' && s[i] != '1'&& s[i] != '2'&& s[i] != '3'&& s[i] != '4'
+	{
+		if(s[i]>='0' && s[i]<='9');
+		else
+		{
+			return temp=false;
+			break;
+		}
+
+	}
+		/*if (s[i] != '0' && s[i] != '1'&& s[i] != '2'&& s[i] != '3'&& s[i] != '4'
 			&& s[i] != '5'&& s[i] != '6'&& s[i] != '7'&& s[i] != '8'&& s[i] != '9')
 		{
 			return false;
-		}
+		}*/
 
 		wX=atoi(s);
 
 		if(wX <= 0)
 		{
-			return false;
+			return temp=false;
 		}
 		
-		return true;
+		return temp;
 }
 
 
