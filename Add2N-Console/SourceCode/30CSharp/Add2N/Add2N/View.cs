2d13267da41b758eda589604c259f62221da45a6
giangchau92 <giangchau92@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-21 16:11:39 +0000

    Thêm hàm Ouput (file View.cs)

--- Add2N-Console/SourceCode/30CSharp/Add2N/Add2N/View.cs
+++ Add2N-Console/SourceCode/30CSharp/Add2N/Add2N/View.cs
@@ -11,6 +11,18 @@ namespace Add2N
         {
         }
 
+        /*
+         * Hàm xuất kết quả ra màn hình
+         */
+        public void Output(List<int> sum)
+        {
+            string temp1 = "";
+            for (int i = 0; i < sum.Count; i++)
+            {
+                temp1 = sum[i].ToString() + temp1;
+            }
+            Console.WriteLine("Ans = {0}", temp1);
+        }
         private bool IsValid(string strnumber)
         {
             for (int i = 0; i < strnumber.Count(); i++)
