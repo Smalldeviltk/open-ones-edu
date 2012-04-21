a593bf8ee2cb43a4d80608d8674b9d4148b06f61
HIEPKHOA10520418@gmail.com <HIEPKHOA10520418@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-21 14:43:18 +0000

    tui code class Number voi ham kiem tra roi do

--- trunk/Add2N-Console/SourceCode/12CPlus/Add2N/Add2N/Number.h
+++ trunk/Add2N-Console/SourceCode/12CPlus/Add2N/Add2N/Number.h
@@ -1,4 +1,30 @@
-/*************************************************************
-                        Name: Number.h
-                Describe: Create class StringNumber
+
+/**************************************************************
+**********			Module Name: Number.h			***********
+**********	Describe: Create class StringNumber		***********
 ***************************************************************/
+
+
+#include "iostream"
+#include "string.h"
+
+using namespace std;
+/********************************************************************
+*********************************************************************/
+
+class StringNumber {	//	StringNumber is a number of type string
+public:
+	char* cValue;		// cursor save address of nummber
+
+	//***************************************************************
+	//  function test data input
+	bool Test()
+	{
+		for(int i=0; i<strlen(this->cValue);i++)
+			if(this->cValue[i]<'0' || this->cValue[i]>'9')
+			return false;
+		return true;
+	}
+
+	//***************************************************************
+};
