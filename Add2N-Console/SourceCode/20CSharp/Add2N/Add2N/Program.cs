981a101e9ded37919b5e58dd55f5e2807bd42d81
chuongdanguit <chuongdanguit@gmail.com>
2012-04-21 14:38:00 +0000

    Mình post thêm code hàm Add nha, các bạn xem thử...

--- Add2N-Console/SourceCode/20CSharp/Add2N/Add2N/Program.cs
+++ Add2N-Console/SourceCode/20CSharp/Add2N/Add2N/Program.cs
@@ -13,6 +13,36 @@ namespace Add2N
         }
 
         /// <summary>
+        /// Do the Sum of large numbers declare by String
+        /// </summary>
+        /// <param name="num1">First number</param>
+        /// <param name="num2">Second number</param>
+        /// <returns>Sum of numbers</returns>
+        static string Add(string num1, string num2)
+        {
+            Stack myStack1 = new Stack();
+            Stack myStack2 = new Stack();
+            for (int i = 0; i < num1.Length; i++)
+                myStack1.Push(byte.Parse(num1.Substring(i, 1)));
+            for (int j = 0; j < num2.Length; j++)
+                myStack2.Push(byte.Parse(num2.Substring(j, 1)));
+            int mNum = 0;
+            string returnNum = "";
+            while (true)
+            {
+                int n1, n2;
+                n1 = myStack1.Count > 0 ? (byte)myStack1.Pop() : 0;
+                n2 = myStack2.Count > 0 ? (byte)myStack2.Pop() : 0;
+                int tong = n1 + n2 + mNum;
+                if (tong == 0)
+                    break;
+                mNum = tong > 9 ? 1 : 0;
+                returnNum = (tong > 9 ? tong - 10 : tong).ToString() + returnNum;
+            }
+            return returnNum;
+        }
+
+        /// <summary>
         /// Check String that is full of character numbers
         /// </summary>
         /// <param name="num">String input</param>
