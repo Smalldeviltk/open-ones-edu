cac10fb3fa56a66c578910eb248ea21bd602ad3c
ntthangnt@gmail.com <ntthangnt@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-21 10:09:41 +0000

    Xong cong 2 List<int> (file Calculate.cs)

--- trunk/Add2N-Console/SourceCode/30CSharp/Add2N/Add2N/Calculate.cs
+++ trunk/Add2N-Console/SourceCode/30CSharp/Add2N/Add2N/Calculate.cs
@@ -7,5 +7,35 @@ namespace Add2N
 {
     class Calculate
     {
+        public Calculate()
+        {
+        }
+        public List<int> Sum(List<int> listNumber1, List<int> listNumber2)
+        {
+            List<int> listSum = new List<int>();
+            int max = (listNumber1.Count > listNumber2.Count ? listNumber1.Count : listNumber2.Count);
+            int re = 0;
+            for (int i = listNumber1.Count; i <= max; i++)
+                listNumber1.Add(0);
+            for (int i = listNumber2.Count; i <= max; i++)
+                listNumber2.Add(0);
+            for (int i = 0; i <= max; i++)
+            {
+                int temp = listNumber1[i] + listNumber2[i] + re;
+                re = 0;
+                if (temp >= 10)
+                {
+                    temp = temp % 10;
+                    re = 1;
+                }
+                listSum.Add(temp);
+            }
+            if (listSum[listSum.Count - 1] == 0)
+            {
+                listSum.RemoveAt(listSum.Count - 1);
+            }
+
+            return listSum;
+        }
     }
 }
