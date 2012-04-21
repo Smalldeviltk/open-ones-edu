d9dd7f5e4e4fda65d3fa4fb91c681f6a3b5c61dc
lightoflife22@gmail.com <lightoflife22@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-21 10:20:15 +0000

    Da them IsValid (file View.cs)

--- trunk/Add2N-Console/SourceCode/30CSharp/Add2N/Add2N/View.cs
+++ trunk/Add2N-Console/SourceCode/30CSharp/Add2N/Add2N/View.cs
@@ -7,5 +7,20 @@ namespace Add2N
 {
     class View
     {
+        public View()
+        {
+        }
+
+        private bool IsValid(string strnumber)
+        {
+            for (int i = 0; i < strnumber.Count(); i++)
+            {
+                if (!((strnumber[i] < 58) && (strnumber[i] > 47)))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
