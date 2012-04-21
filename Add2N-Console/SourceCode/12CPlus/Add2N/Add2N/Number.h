2d4756c89edc1353216c34f9c5f3f15e258e1757
HIEPKHOA10520418 <HIEPKHOA10520418@gmail.com>
2012-04-21 14:55:34 +0000

    minh up toan bo file Number.h mình vua code. các ban xem có sua gi khong. roi viet ham Main

--- Add2N-Console/SourceCode/12CPlus/Add2N/Add2N/Number.h
+++ Add2N-Console/SourceCode/12CPlus/Add2N/Add2N/Number.h
@@ -27,4 +27,67 @@ public:
 	}
 
 	//***************************************************************
+	//	oveload operator ">>" using to add input value of number
+	friend istream& operator>>(istream& is, StringNumber &stringNum)
+	{
+		stringNum.cValue=new char[100];
+		is>>stringNum.cValue;
+		return is;
+	}
+	
+	//***************************************************************
+	//	overload operator "<<" using to print number on screen
+	friend ostream& operator<<(ostream& os, StringNumber stringNum)
+	{
+		os<<stringNum.cValue;
+		return os;
+	}
+
+	//***************************************************************
+	//	oveload operator "+" using to use to add 2 number
+	friend StringNumber operator+(StringNumber stringNum1, StringNumber stringNum2)
+	{
+		StringNumber snResult;	// variable save result
+		char cRemember=0;		// variable save some memory
+		
+		int iLen1=strlen(stringNum1.cValue);	// length of first number
+		int iLen2=strlen(stringNum2.cValue);	// length of second number
+		int iLen=0;								// length of result
+
+		// to issue memory to variable result
+		if(iLen1>=iLen2)
+			snResult.cValue=new char[iLen1];
+		else
+			snResult.cValue=new char[iLen2];
+
+		// start add from the unit
+		while((iLen1>0) && (iLen2>0))
+		{
+			char cSum=stringNum1.cValue[--iLen1]+stringNum2.cValue[--iLen2] -2*'0'+cRemember;
+			snResult.cValue[iLen++]=cSum%10+'0';
+			cRemember=cSum/10;
+		}
+
+		// when second number end
+		while(iLen1>0)
+		{
+			char cSum=stringNum1.cValue[--iLen1]-'0'+cRemember;
+			snResult.cValue[iLen++]=cSum%10+'0';
+			cRemember=cSum/10;
+		}
+		// when first number end
+		while(iLen2>0)
+		{
+			char cSum=stringNum2.cValue[--iLen2]-'0'+cRemember;
+			snResult.cValue[iLen++]=cSum%10+'0';
+			cRemember=cSum/10;
+		}
+
+		if(cRemember) snResult.cValue[iLen++]='1';
+		snResult.cValue[iLen]=0;
+		
+		// reverse result
+		strrev(snResult.cValue);
+		return snResult;
+	}
 };
