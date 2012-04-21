171e7f30003d280196c7ef1b3b026146994bbf2e
phuchy220284 <phuchy220284@gmail.com>
2012-04-21 03:42:30 +0000

    mình đã làm checkNumString để kiểm tra chuỗi nhập vào có phải là số không

--- Add2N-Console/SourceCode/20CSharp/Add2N/Add2N/Program.cs
+++ Add2N-Console/SourceCode/20CSharp/Add2N/Add2N/Program.cs
@@ -9,6 +9,22 @@ namespace Add2N
     {
         static void Main(string[] args)
         {
+
+        }
+
+        /// <summary>
+        /// Check String that is full of character numbers
+        /// </summary>
+        /// <param name="num">String input</param>
+        /// <returns>true if this string is a numbers string\n else return false</returns>
+        static bool checkNumString(string num)
+        {
+            char[] numArray = num.ToCharArray();
+            string strCompare = "0123456789";
+            foreach (char eNum in numArray)
+                if (strCompare.IndexOf(eNum) < 0)
+                    return false;
+            return true;
         }
     }
 }
