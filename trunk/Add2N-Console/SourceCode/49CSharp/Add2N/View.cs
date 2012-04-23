b9da1fbb203b66ec4e8a35cf907648f204883dce
nkt1321991@gmail.com <nkt1321991@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-23 15:25:19 +0000

    Hiện tại mình đã viết cấu trúc tổng quát cho bài này! Class View dùng để kiểm tra lỗi chuỗi nhập vào và xuất kết quả cho người dùng. Class BigInteger dùng để thực hiện phép toán logic. Mình sẽ phân chia công việc như sau: Long và mình sẽ xử lý việc cộng 2 số lớn trong class BigInteger. Tú và Cường sẽ thực hiện việc kiểm tra lỗi đầu vào của chương trình. Anh Hoài thực hiện việc xuất kết quả ra mà hình. Chi tiết cụ thể mình sẽ gửi mail cho mọi người. Thân!

---
+++ trunk/Add2N-Console/SourceCode/49CSharp/Add2N/View.cs
@@ -0,0 +1,58 @@
+﻿//////////////////////////////////////////////////////////////////////////
+///
+/// FileName: View.cs
+/// 
+/// Version:  1.0
+/// 
+/// Created Date: 23/04/2012
+/// 
+/// Copyright: DTHT.Group49
+/// 
+/// Modification Logs:
+///     DATE            AUTHOR              DESCRIPTION
+/// -----------------------------------------------------
+///  23/04/2012     Nguyen Khac Trung       Creating
+///
+//////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Add2N
+{
+    class View
+    {
+        // Input
+        private string[] m_str;
+
+        // Initialization
+        public View(string[] _str)
+        {
+            m_str = _str;
+        }
+
+        /// <summary>
+        /// Check error for input
+        /// </summary>
+        /// <param name="_bigInteger1"></param>
+        /// <param name="_bigInteger2"></param>
+        /// <returns></returns>
+        public bool IsHaveError(ref BigInteger _bigInteger1, ref BigInteger _bigInteger2)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Export result for user
+        /// </summary>
+        /// <param name="_number1"> number 1 </param>
+        /// <param name="_number2"> number 2 </param>
+        /// <param name="_result"> result </param>
+        public void ExportResult(string _number1, string _number2, string _result)
+        {
+        }
+    }
+}
