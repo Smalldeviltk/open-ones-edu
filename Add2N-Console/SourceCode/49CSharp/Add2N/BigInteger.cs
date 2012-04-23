ab4e3507df15fb8ad359c6b7a6ae3f3e1053b1eb
nkt1321991 <nkt1321991@gmail.com>
2012-04-23 15:25:19 +0000

    Hiện tại mình đã viết cấu trúc tổng quát cho bài này! Class View dùng để kiểm tra lỗi chuỗi nhập vào và xuất kết quả cho người dùng. Class BigInteger dùng để thực hiện phép toán logic. Mình sẽ phân chia công việc như sau: Long và mình sẽ xử lý việc cộng 2 số lớn trong class BigInteger. Tú và Cường sẽ thực hiện việc kiểm tra lỗi đầu vào của chương trình. Anh Hoài thực hiện việc xuất kết quả ra mà hình. Chi tiết cụ thể mình sẽ gửi mail cho mọi người. Thân!

---
+++ Add2N-Console/SourceCode/49CSharp/Add2N/BigInteger.cs
@@ -0,0 +1,53 @@
+﻿//////////////////////////////////////////////////////////////////////////
+///
+/// FileName: BigIntger.cs
+/// 
+/// Version:  1.0
+/// 
+/// Created Date: 23/04/2012
+/// 
+/// Copyright: DTHT.Group49
+/// 
+/// Modification Logs:
+///     DATE            AUTHOR              DESCRIPTION
+/// -----------------------------------------------------
+///  23/04/2012     Nguyen Khac Trung       Creating
+///
+//////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Add2N
+{
+    class BigInteger
+    {
+        // Big Integer
+        string m_bInteger;
+
+        // Get and set variable m_bInteger
+        public string BInteger
+        {
+            get { return m_bInteger; }
+            set { m_bInteger = value; }
+        }
+
+        // Initialization
+        public BigInteger()
+        {
+            m_bInteger = null;
+        }
+
+        /// <summary>
+        /// Add 2 big integer number 
+        /// </summary>
+        /// <param name="_bigInteger"> number will be added</param>
+        /// <returns> result </returns>
+        public string Add2BigInteger(BigInteger _bigInteger)
+        {
+            return null;
+        }    
+    }
+}
---
+++ Add2N-Console/SourceCode/49CSharp/Add2N/Program.cs
@@ -0,0 +1,48 @@
+﻿//////////////////////////////////////////////////////////////////////////
+///
+/// FileName: Program.cs
+/// 
+/// Version:  1.0
+/// 
+/// Created Date: 23/04/2012
+/// 
+/// Copyright: DTHT.Group49
+/// 
+/// Modification Logs:
+///     DATE            AUTHOR              DESCRIPTION
+/// -----------------------------------------------------
+///  23/04/2012     Nguyen Khac Trung       Creating
+///
+//////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Add2N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialization
+            View view = new View(args);
+            BigInteger bigInteger1 = new BigInteger();
+            BigInteger bigInteger2 = new BigInteger();
+
+            Console.WriteLine("*-----------------------------*");
+
+            // Continue if having error
+            if (view.IsHaveError(ref bigInteger1, ref bigInteger2))
+            {
+                return; // Exit programing
+            }
+            else
+            {
+                //Export result for user
+                view.ExportResult(bigInteger1.BInteger, bigInteger2.BInteger, bigInteger1.Add2BigInteger(bigInteger2));
+            }
+        }
+    }
+}
