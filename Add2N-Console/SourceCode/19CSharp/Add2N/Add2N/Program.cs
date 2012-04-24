6968ca0973b8d2f1cc84891248d01fa2d0a461e1
dzunhd <dzunhd@gmail.com>
2012-04-24 02:34:28 +0000

    Dung(10520581) nop phan Program.cs

---
+++ Add2N-Console/SourceCode/19CSharp/Add2N/Add2N/Program.cs
@@ -0,0 +1,46 @@
+﻿#region Copyright Group 19
+//
+// All rights are reserved. Reproduction or transmission in whole or 
+// in part, in any form or by any means, electronic, mechanical or
+// otherwise, is prohibited without the prior written consent of the 
+// copyright owner.
+// Filename: Program.cs
+//
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Add2N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+                try
+                {
+                    View.TitleIntro();
+                    string strChoice;
+                    do
+                    {
+                        View.Output(Logic.Calculator());
+                        Console.WriteLine("\nBan co muon tiep tuc khong?");
+                        Console.Write("\t\tChon (Yes=1/No=0): ");
+                        strChoice=Console.ReadLine().Trim();
+                    }
+                    while (strChoice.Equals("1") || strChoice.ToLowerInvariant().Equals("yes"));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Loi:" + e.Message);
+                }
+                finally
+                {
+                    Console.ReadKey();
+                }
+        }
+        ///<summary>
+        ///<summary>
+    }
+}
