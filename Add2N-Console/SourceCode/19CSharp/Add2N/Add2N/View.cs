9801074ee511f67cee592654bc4d29f6ecde6a70
le.ngoisaophale.tham134 <le.ngoisaophale.tham134@gmail.com>
2012-04-24 02:59:56 +0000

    Nộp class View.cs

---
+++ Add2N-Console/SourceCode/19CSharp/Add2N/Add2N/View.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright Group 19
+//
+// All rights are reserved. Reproduction or transmission in whole or 
+// in part, in any form or by any means, electronic, mechanical or
+// otherwise, is prohibited without the prior written consent of the 
+// copyright owner.
+// Filename: View.cs
+//
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Add2N
+{
+    class View
+    {
+        public static void TitleIntro()
+        {
+            Console.WriteLine(" ---------------------------------------------------");
+            Console.WriteLine("|        CHUONG TRINH CONG HAI SO NGUYEN LON        |");
+            Console.WriteLine("| Cu phap chuong trinh: Add2N <thamso1> <thamso2>   |");
+            Console.WriteLine(" ---------------------------------------------------");
+        }
+        public static string[] Input()
+        {
+            string strTemp;
+            string[] temp;
+            Console.Write("Nhap: ");
+            strTemp = Console.ReadLine().Trim();
+            temp = strTemp.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return temp;
+        }
+        public static void Output(string strResult)
+        {
+            Console.WriteLine(strResult);
+        } 
+    }
+}
