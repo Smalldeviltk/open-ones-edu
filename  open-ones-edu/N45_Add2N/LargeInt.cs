79919a4e8e38c7513e3d6c1a3f2d149b747f9306
Nguyen.bac93 <Nguyen.bac93@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-24 07:03:20 +0000

    Mình thêm vào class số nguyên lớn phương thức cộng

---  open-ones-edu/N45_Add2N/LargeInt.cs
+++  open-ones-edu/N45_Add2N/LargeInt.cs
@@ -45,5 +45,51 @@ namespace N45_Add2N
                 return IsLInt;
             }
         }
+
+        //Khởi tạo một số nguyên lớn bằng 1 chuỗi có sẳn
+        public LargeInt(string largeInt)
+        {
+            Large_Int = "";
+            Large_Int = largeInt;
+        }
+
+        //Phương thức đảo chuỗi
+        public string Rev(string largeInt)
+        {
+            string temp = "";
+            for (int i = largeInt.Length - 1; i >= 0; i--)
+            {
+                temp += largeInt[i];
+            }
+            return temp;
+        }
+
+        //Phương thức cộng 2 số nguyên trả về 1 số nguyên
+        public static LargeInt operator +(LargeInt a, LargeInt b)
+        {
+            int Count = a.Large_Int.Length > b.Large_Int.Length ? a.Large_Int.Length : b.Large_Int.Length;
+            int res = 0;
+            string temp = "";
+            int x1, x2;
+            string tempA = a.Rev(a.Large_Int);
+            string tempB = b.Rev(b.Large_Int);
+
+            //Cộng 2 số nguyên lớn
+            if (a.IsLargeInt && b.IsLargeInt)
+            {
+                for (int i = 0; i < Count; i++)
+                {
+                    x1 = x2 = 0;
+                    if (tempA.Length > i) x1 = Char.ConvertToUtf32(tempA, i) - 48;
+                    if (tempB.Length > i) x2 = Char.ConvertToUtf32(tempB, i) - 48;
+
+                    temp = ((x1 + x2 + res) % 10).ToString() + temp;
+                    res = (x1 + x2 + res) / 10;
+                }
+            }
+            else
+                temp = "0";
+            return new LargeInt(temp);
+        }
     }
 }
