0ebdc9b1353bd7e031bb614a8eae0bc1a2ffad6a
cmt1905 <cmt1905@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-24 07:25:57 +0000

    Sửa lại file AssemblyInfo.cs

--- trunk/Add2N-Console/SourceCode/19CSharp/Add2N/Add2N/Properties/AssemblyInfo.cs
+++ trunk/Add2N-Console/SourceCode/19CSharp/Add2N/Add2N/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Add2Nss")]
+[assembly: AssemblyTitle("Add2N")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Add2Nss")]
+[assembly: AssemblyProduct("Add2N")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
