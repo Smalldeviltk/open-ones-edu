c5c8c8b6395f3e8eee62140d0403b14ca0949ad4
bladevn23 <bladevn23@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-25 12:48:27 +0000

    Vũ Thanh Hoà bladevn23@gmail.com up lớp View.cs

---
+++ trunk/Add2N-Console/SourceCode/15CSharp/Add2N/Add2N/View.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Add2N
+{
+    class View
+    {
+        public View()
+        {
+        }
+        public void In(ref string _number)
+        {
+            Console.Write("Insert Number: ");
+            _number = Console.ReadLine();
+        }
+        public void Out(string _number)
+        {
+            Console.WriteLine("" + _number);
+        }
+    }
+}
