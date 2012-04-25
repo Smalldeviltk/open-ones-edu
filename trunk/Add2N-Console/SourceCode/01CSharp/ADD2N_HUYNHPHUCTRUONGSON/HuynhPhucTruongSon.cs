46cbe045ddabc6c2f8bcebea67a91bebe09ebe8f
loveyouth.kiemphong@gmail.com <loveyouth.kiemphong@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-25 14:55:51 +0000

    Huynh Phuc Truowng Son

---
+++ trunk/Add2N-Console/SourceCode/01CSharp/ADD2N_HUYNHPHUCTRUONGSON/HuynhPhucTruongSon.cs
@@ -0,0 +1,29 @@
+﻿static bool CheckRef(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9' || c == '-')
+                {
+                    Console.WriteLine("<*> DOI SO '" + str + "' KHONG HOP LE ");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool CheckCountRef(int index)
+        {
+            if (index > 2 )
+            {
+                Console.WriteLine("\n=========   THONG-BAO-LOI    ==========");
+                Console.WriteLine("======   VUOT QUA HAI DOI SO   ========");
+                return false;
+            }
+            else if (index == 1)
+            {
+                Console.WriteLine("=========   THONG-BAO-LOI    ==========");
+                Console.WriteLine("======= MOT DOI SO SAO MA TINH ========");
+                return false;
+            }
+            return true;
+        }
