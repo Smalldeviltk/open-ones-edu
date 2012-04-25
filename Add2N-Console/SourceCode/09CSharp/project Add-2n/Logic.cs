58ca95f0c259dd8716bed4303d74d891f0e0b216
lich533 <lich533@gmail.com>
2012-04-25 15:54:03 +0000

    them class Logic.cs

---
+++ Add2N-Console/SourceCode/09CSharp/project Add-2n/Logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Add_2n
+{
+    class Logic
+    {
+        //Kiem tra du lieu nhap co hop le hay khong???
+        //  Hop le tr ve true, nguoc lai la false
+        public Boolean isNumber(String s)
+        {
+            foreach (Char c in s)
+            {
+                if (!Char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
