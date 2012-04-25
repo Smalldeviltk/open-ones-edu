c751338356615b8f8937af2afcdeaf45acebefad
xuantungpm1@gmail.com <xuantungpm1@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-25 15:38:49 +0000

    Tùng đã add View.cs rồi, và viết phần đầu, Nhân viết thêm method hiển thị kết quả là được.

---
+++ trunk/Add2N-Console/SourceCode/09CSharp/project Add-2n/View.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Add_2n
+{
+    public class View
+    {
+        private String s1;
+        private String s2;
+
+        public View()
+        {
+            s1 = "";
+            s2 = "";
+        }
+
+        //Gan gia tri x, y cho gia tri s1, s2 cua lop (s1.Length > s2.Length)
+        public void setNumber(String x, String y)
+        {
+            if (x.Length > y.Length)
+            {
+                this.s1 = x;
+                this.s2 = y;
+            }
+            else
+            {
+                this.s1 = y;
+                this.s2 = x;
+            }
+        }
+
+    }
+}
