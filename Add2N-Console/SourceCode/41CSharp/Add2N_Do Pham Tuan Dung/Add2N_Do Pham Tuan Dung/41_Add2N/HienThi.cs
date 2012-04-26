6a0c5489da3dbc98bc81eec7b4690b5565158c1e
ittuandung <ittuandung@gmail.com>
2012-04-26 14:05:14 +0000

    a

---
+++ Add2N-Console/SourceCode/41CSharp/Add2N_Do Pham Tuan Dung/Add2N_Do Pham Tuan Dung/41_Add2N/HienThi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _41_Add2N
+{
+    class HienThi
+    {
+        // Hàm giới thiệu chương trình
+        public static void _GioiThieu()
+        {
+            Console.WriteLine("\n\n-------- Plus 2 Prime Large Program - GROUP 41 ---------");
+            Console.WriteLine("Cu phap: Add2N [Num1] [Num2]");
+        }
+
+        //Hàm nhập thông tin, tách chuỗi và trả về chuỗi cơ bản.
+        public static string[] _NhapThongTin()
+        {
+            string strTempt;
+            string[] temp;
+            Console.Write("Console: ");
+            strTempt = Console.ReadLine().Trim();
+            temp = strTempt.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return temp;
+        }
+
+        //Hàm hiển thị thông tin 
+        public static void ThongTin(string strResult)
+        {
+            Console.WriteLine(strResult);
+        }
+    }
+}
