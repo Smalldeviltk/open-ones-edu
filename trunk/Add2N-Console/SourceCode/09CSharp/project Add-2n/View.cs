71f209e6874f0b5d5f252fde35f881cbccfa339d
huunhan18pro@gmail.com <huunhan18pro@gmail.com@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-26 10:24:49 +0000

    Thêm Hàm Xuất Kết Quả:

--- trunk/Add2N-Console/SourceCode/09CSharp/project Add-2n/View.cs
+++ trunk/Add2N-Console/SourceCode/09CSharp/project Add-2n/View.cs
@@ -33,11 +33,12 @@ namespace Add_2n
 
         //Hien thi ket qua ra man hinh
         //  Lay phuong thuc add2N cua class Logic de tinh toan
+        
         public void showResult(String x, String y)
         {
             Logic bNumber = new Logic();
             String s = bNumber.add2N(this.s1, this.s2);
-            Console.WriteLine("\tKet qua: {0} + {1} = {2}", x, y, s);
+            Console.WriteLine("\tKết Quả: {0} + {1} = {2}", x, y, s);
         }
 
     }
