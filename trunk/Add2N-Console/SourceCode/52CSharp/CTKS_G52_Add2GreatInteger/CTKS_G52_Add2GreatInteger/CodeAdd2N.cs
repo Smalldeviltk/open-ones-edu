aeff94c27c28cb075b8dd5b17a8fd8a18be2e895
doxuanlap92 <doxuanlap92@a162e803-6004-09d3-3d29-f63154ef460b>
2012-04-26 04:39:15 +0000

    Cài đặt hàm ErrorType() //chưa thử

--- trunk/Add2N-Console/SourceCode/52CSharp/CTKS_G52_Add2GreatInteger/CTKS_G52_Add2GreatInteger/CodeAdd2N.cs
+++ trunk/Add2N-Console/SourceCode/52CSharp/CTKS_G52_Add2GreatInteger/CTKS_G52_Add2GreatInteger/CodeAdd2N.cs
@@ -21,7 +21,7 @@ namespace CTKS_GreatInteger
         public string Code { get; set; }
         public string Key { get { return "Add2N"; } }//keyword giup nhan dien loai command
 
-        decimal ad;
+        
 
         //constructor
         public CodeAdd2N()
@@ -36,24 +36,85 @@ namespace CTKS_GreatInteger
         }
 
 
+        //Get number 1
+        public string Number1()
+        {
+            int head = Code.IndexOf(" ") + 1; //vị trí kí tự đầu của chuỗi số 1(đứng sau space thứ 1)
+            int tail = Code.IndexOf(" ", head); //vị trí cuối của chuỗi số 1 (đứng trước space thứ 2)
+            return Code.Substring(head, tail - head);
+        }
+
+        //Get number 2
+        public string Number2()
+        {
+            int head = Code.IndexOf(" ", Code.IndexOf(" ")) + 1;
+            int tail = Code.Length;
+            return Code.Substring(head, tail - head);
+        }
+
         //get Error type
         public Add2NError ErrorType()
         {
             Add2NError Error = new Add2NError();
+
             Error.KeyWord = false;
             Error.Num1 = false;
             Error.Num2 = false;
             Error.AllNum = false;
             Error.NoError = false;
 
+            //wrong if the other 2 spacious
+            int space = 0;
+            for (int i = 0; i < Code.Length; i++)
+            {
+                if (Code[i] == ' ')
+                {
+                    space++;
+                }
+            }
+            if (space != 2||Code.Substring(0,5)!="Add2N")
+            {
+                Error.KeyWord = true;
+            }
+            else
+            {
+                //Number 1 is wrong
+                string num1 = Code.Substring(Code.IndexOf(" ") + 1, Code.IndexOf(" ", Code.IndexOf(" ") + 1) - Code.IndexOf(" ", Code.IndexOf(" ")) + 1);
+                for (int i = 0; i < num1.Length; i++)
+                {
+                    if ((num1[i] < '0') || (num1[i] > '9'))
+                    {
+                        Error.Num1 = true;
+                    }
+                }
 
-            return Error;
+                //Number 2 is wrong
+                string num2=Code.Substring(Code.IndexOf(" ", Code.IndexOf(" ")) + 1, Code.Length - Code.IndexOf(" ", Code.IndexOf(" ")) + 1);
+                for (int i = 0; i < num2.Length; i++)
+                {
+                    if ((num2[i]<'0')||(num2[i]>'9'))
+                    {
+                        Error.Num2 = true;
+                    }
+                }
+                
+            }
+            if (Error.Num1 && Error.Num2)
+            {
+                Error.AllNum = true;
+            }
+            if((!Error.KeyWord)&&(!Error.Num1)&&(!Error.Num2)&&(!Error.AllNum))
+            {
+                Error.NoError = true;
+            }
+
+                return Error;
         }
 
         //the code Wrong or Right
         public bool Valid()
         {
-            return true;
+            return  ErrorType().NoError;
         }
 
         //get Error Message
@@ -82,22 +143,6 @@ namespace CTKS_GreatInteger
 
 
 
-        //Get number 1
-        public string Number1()
-        {
-            int head = Code.IndexOf(" ") + 1; //vị trí kí tự đầu của chuỗi số 1(đứng sau space thứ 1)
-            int tail = Code.IndexOf(" ", head); //vị trí cuối của chuỗi số 1 (đứng trước space thứ 2)
-            return Code.Substring(head, tail - head);
-        }
-
-        //Get number 2
-        public string Number2()
-        {
-            int head = Code.IndexOf(" ", Code.IndexOf(" ")) + 1;
-
-            int tail = Code.Length;
-
-            return Code.Substring(head, tail - head);
-        }
+     
     }
 }
